0e77c1e2f623c5480d99827bc05091e7e40c56d1
Mark Schneider <schneider.mark14@gmail.com>
2026-03-24 19:51:04 +0300

    Switch smoke WTMAD subset to W4-11-30

--- test_models/experiment.py
+++ test_models/experiment.py
@@ -10,7 +10,7 @@ from threading import Lock
 
 from common import EXPERIMENTS_DIR, ensure_dir, ensure_runtime_directories
 
-SMOKE_WTMAD_DATABASES = ("W4-11-132",)
+SMOKE_WTMAD_DATABASES = ("W4-11-30",)
 SMOKE_AVRANE_MOLECULES = ("H2",)
 
 
